ac0860869bcda211be5bff9af6627f08f998d503
Tutu Moses <etu.moses@gmail.com>
2025-12-14 14:53:30 +0000

    delete

--- frontend/realtime-transcriber/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function Page() {
-  redirect('/transcribe/realtime-transcriber-01')
-}
--- frontend/voice-agent/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function Page() {
-  redirect('/voice/voice-chat')
-}
